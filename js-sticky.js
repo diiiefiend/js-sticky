28c94286495588e1d18858ec4ac0fd67c1f7d1e5
Ariel <arielqhou@gmail.com>
2016-05-21 20:04:49 -0400

    Make scroller behavior more robust (if scroller element isn't position: fixed to begin with).

--- js-sticky.js
+++ js-sticky.js
@@ -1,34 +1,57 @@
 // TO USE:
-// in your document, define #scroller and #scroller-anchor
+// in your document, define #scroller and #scroller-anchor (and possibly #scroller-cont)
 // to change trigger from top of #scroller to bottom of #scroller, use #scroller-bottom instead of #scroller
+// define a container div #scroller-cont if #scroller isn't a position: fixed or position: absolute element
 
 function moveScroller(trigger) {
   var move, scrollEl, scrollPos;
 
   var anchorPos = $("#scroller-anchor").offset().top; // always static
 
+  var scrollEl = (trigger === "top") ? $("#scroller") : $("#scroller-bottom");
+
+  // total = with margins
+  var elTotalWidth = scrollEl.outerWidth(true);
+  var elTotalHeight = scrollEl.outerHeight(true);
+  // doesn't include margins
+  var elHeight = scrollEl.outerHeight();
+  // just the width of the inner content (no padding/border/margins)
+  var elInnerWidth = scrollEl.css("width");
+
   if (trigger === "top"){
-    scrollEl = $("#scroller");
 
     move = function() {
       scrollPos = $(window).scrollTop();
       if (scrollPos > anchorPos) {
         scrollEl.css({
             position: "fixed",
-            top: "30px"
+            top: "0",             // change as needed
+            width: elInnerWidth,
+            zIndex: 100,
+            opacity: 0.9
         });
+
+        // need to create a "placeholder" for the element in the DOM if it
+        // used to be position: static or position: relative
+        if(document.getElementById("scroller-cont") !== null){
+          $("#scroller-cont").css({
+            width: elTotalWidth,
+            height: elTotalHeight
+          });
+        }
+
       } else {
         scrollEl.css({
-            position: "relative",
-            top: ""
+            position: "",
+            top: "",
+            width: "",
+            zIndex: "",
+            opacity: ""
         });
       }
     };
   } else if (trigger === "bottom"){
     var initialScrollPos;
-    scrollEl = $("#scroller-bottom");
-    var elHeight = scrollEl.outerHeight();
-    var elWidth = scrollEl.outerWidth();
     var stickied = false;
 
     move = function() {
@@ -43,7 +66,6 @@ function moveScroller(trigger) {
             top: "",
             width: ""
         });
-        // console.log("unstuck!");
         stickied = false;
         initialScrollPos = undefined;
       }
@@ -61,8 +83,7 @@ function moveScroller(trigger) {
         scrollEl.css({
             position: "absolute",
             top: anchorPos - elHeight - 250,
-            // need this bc position: absolute shrinks the width sometimes for some reason
-            width: elWidth
+            width: elInnerWidth
         });
       }
     };
@@ -72,8 +93,8 @@ function moveScroller(trigger) {
   move();
 }
 
-// on document load, look for scroller elements
-$(function() {
+// on page load, look for scroller elements
+$(document).on("pageLoaded", function() {
   if(document.getElementById("scroller") !== null){
     moveScroller("top");
   }
